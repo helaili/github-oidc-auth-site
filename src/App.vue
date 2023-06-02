1fb2b69f397385f8f1195f9adb840b13d1400a5c
Alain Hélaïli <alain@carbonhub.io>
2023-06-02 15:13:34 +0200

    do we need an explicit link?

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import Introduction from './components/Introduction.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/try">Try</RouterLink>
         <RouterLink to="/deploy">Deploy</RouterLink>
+        <RouterLink to="/success">Success</RouterLink>
       </nav>
     </div>
   </header>
