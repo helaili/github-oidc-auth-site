97e2cda826934fc57e611a824c96e4333093fd82
Alain Hélaïli <alain@carbonhub.io>
2023-06-02 15:15:31 +0200

    Remove that link

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@ import Introduction from './components/Introduction.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/try">Try</RouterLink>
         <RouterLink to="/deploy">Deploy</RouterLink>
-        <RouterLink to="/success">Success</RouterLink>
       </nav>
     </div>
   </header>
