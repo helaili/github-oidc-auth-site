618abbb04db3eb4d84337e21dd2e7b9c45a12522
Alain Hélaïli <alain@carbonhub.io>
2023-06-02 15:18:36 +0200

    hide link

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import Introduction from './components/Introduction.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/try">Try</RouterLink>
         <RouterLink to="/deploy">Deploy</RouterLink>
+        <RouterLink v-show="false" to="/success">Success</RouterLink>
       </nav>
     </div>
   </header>
