cbeb4cab5627eb164368b38c71a258e8cd0e9605
Alain Hélaïli <alain@carbonhub.io>
2023-06-05 13:49:32 +0200

    redirect from home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,22 @@
 import Welcome from '../components/WelcomeList.vue'
 </script>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({ 
+  mounted() {
+    let code = this.$route.query.code?.toString() || ''
+    
+    if (code !== '') {  
+      // Because of how the router works, a direct call from the outside to /success will not work.
+      // We need to hit the index.html page, which loads the router, so that /success can be resolved.
+      this.$router.push({ path: '/success', query: { code: code } })
+    }
+  }
+})
+</script>
+
 <template>
   <main>
     <Welcome />
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-oidc-auth-site/',
+  //base: '/github-oidc-auth-site/',
   plugins: [vue()],
   resolve: {
     alias: {
